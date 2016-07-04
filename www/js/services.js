a06d60edb67e939a1b83dd8d16822bdb4656ad24
aloo <aloocs@gmail.com>
2016-07-04 18:32:33 +0800

    修改成了leancloud的api

--- www/js/services.js
+++ www/js/services.js
@@ -1,30 +1,30 @@
 // empty services provide api to the application
-angular.module('ionictodo.services',[]).factory('Todo', ['$http','PARSE_CREDENTIALS',
+angular.module('ionictodo.services',[]).factory('Todo', ['$http','LEANCLOUD_CREDENTIALS',
   function($http){
     return {
       getAll:function(){
-        return $http.get('https://api.parse.com/1/classes/Todo',{
+        return $http.get('https://api.leancloud.cn/1.1/classes/ionictodo',{
           headers: {
-            'X-Parse-Application-id': PARSE_CREDENTIALS.APP_ID, //来源传入的PARSE_CREDENTIALS
-            'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
+            'X-LC-Id': LEANCLOUD_CREDENTIALS.APP_ID, //来源传入的LEANCLOUD_CREDENTIALS
+            'X-LC-Sign': LEANCLOUD_CREDENTIALS.REST_API_KEY,
           }
         });
       },
       get: function(id){
-        return $http.get('https://api.parse.com/1/classes/Todo/'+id, {
+        return $http.get('https://api.leancloud.cn/1.1/classes/ionictodo/'+id, {
           headers: {
-            'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-            'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY
+            'X-LC-Id': LEANCLOUD_CREDENTIALS.APP_ID,
+            'X-LC-Sign': LEANCLOUD_CREDENTIALS.REST_API_KEY
           }
         });
       },
       create: function(data){
         return $http.post
         (
-          'https//api.parse.com/1/classes/Todo', data, {
+          'https//api.parse.com/1/classes/ionictodo', data, {
             headers: {
-              'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-              'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
+              'X-LC-Id': LEANCLOUD_CREDENTIALS.APP_ID,
+              'X-LC-Sign': LEANCLOUD_CREDENTIALS.REST_API_KEY,
               'Content-Type':'application/json'
             }
           }
@@ -32,24 +32,27 @@ angular.module('ionictodo.services',[]).factory('Todo', ['$http','PARSE_CREDENTI
       },
       edit: function(id, data){
         return $http.put
-        ('https://api.parse.com/1/classes/Todo'+id, data, {
+        ('https://api.leancloud.cn/1.1/classes/ionictodo'+id, data, {
           headers: {
-            'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-            'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
+            'X-LC-Id': LEANCLOUD_CREDENTIALS.APP_ID,
+            'X-LC-Sign': LEANCLOUD_CREDENTIALS.REST_API_KEY,
             'Content-Type':'application/json'
           }
         })
       },
       delete: function(id){
         return $http.delete
-        ('https://api.parse.com/1/classes/Todo'+id,  {
+        ('https://api.leancloud.cn/1.1/classes/ionictodo'+id,  {
           headers: {
-            'X-Parse-Application-Id': PARSE_CREDENTIALS.APP_ID,
-            'X-Parse-REST-API-Key': PARSE_CREDENTIALS.REST_API_KEY,
+            'X-LC-Id': LEANCLOUD_CREDENTIALS.APP_ID,
+            'X-LC-Sign': LEANCLOUD_CREDENTIALS.REST_API_KEY,
             'Content-Type':'application/json'
           }
         })
       }
     }
   }
-])
+]).value(LEANCLOUD_CREDENTIALS,'LEANCLOUD_CREDENTIALS',{
+  APP_ID : "FFnN2hso42Wego3pWq4X5qlu",
+  REST_API_KEY : "d5bcbb897e19b2f6633c716dfdfaf9be,1453014943466"
+})
